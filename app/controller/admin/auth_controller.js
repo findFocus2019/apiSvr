74a295567acf48d1e7eb44a3db3c09c6040b9321
DESKTOP-P635RSB\Think <zhangyunfeng0101@gmail.com>
2019-03-03 11:12:55 +0800

    feat: add login support

---
+++ app/controller/admin/auth_controller.js
@@ -0,0 +1,54 @@
+/**
+ * 登录，登出
+ */
+
+const Controller = require('./../../../lib/controller')
+const Op = require('sequelize').Op
+
+class AuthController extends Controller {
+  constructor (ctx) {
+    super()
+  }
+
+  /**
+   * 登录
+   * @param {*} ctx 
+   */
+  async login (ctx) {
+    this.logger.info('login: ', ctx.body)
+
+    let adminModel = new this.models.admin_model().model()
+    let body = ctx.body
+
+    let email = body.email
+    let password = body.password
+
+    let admin = await adminModel.findOne({
+      where: {
+        email: email,
+        password: password
+      }
+    })
+
+    if (admin === null) {
+      this.logger.info('login: 账号密码错误' )
+      ctx.ret.data = {code: -1, msg: '账号密码错误'}
+    } else {
+      ctx.session.admin = {email: admin.email, password: admin.password, id: admin.id}
+      ctx.ret.data = {code: 0, msg: 'success', data: {}}
+    }
+  }
+
+  /**
+   * 登出
+   * @param {*} ctx 
+   */
+  async logout (ctx) {
+    this.logger.info('logout: ', ctx.body)
+
+    // 清除session
+    ctx.session.admin = {}
+  }
+}
+
+module.exports = AuthController
