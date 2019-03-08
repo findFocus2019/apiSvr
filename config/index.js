c9fdac6feb4d1bed0f3fcacda3111f85dcbe143a
lucong <332553882@qq.com>
2019-03-08 14:56:30 +0800

    ad

--- config/index.js
+++ config/index.js
@@ -18,8 +18,8 @@ module.exports = {
   // },
 
   db: {
-    host: 'rm-wz9d95e80363zmcjjdo.mysql.rds.aliyuncs.com',
-    // host:'rm-wz9d95e80363zmcjj.mysql.rds.aliyuncs.com',
+    // host: 'rm-wz9d95e80363zmcjjdo.mysql.rds.aliyuncs.com',
+    host:'rm-wz9d95e80363zmcjj.mysql.rds.aliyuncs.com',
     port: 3306,
     dbname: 'find_focus_2019',
     username: 'find_focus_2019',
