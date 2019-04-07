90d90c89fbec6a73c2f33b9ceb2090cf74217eea
lucong <332553882@qq.com>
2019-04-07 22:35:36 +0800

    task

--- tests/schedule.js
+++ tests/schedule.js
@@ -4,10 +4,10 @@ const Schedule = require('../schedule/main')
 
 let schedule = Schedule.getSchedule()
 
-// schedule.fetchNews()
+schedule.fetchNews()
 
 // schedule.rabateDealDay()
 
 // schedule.submitJdOrder()
 
-schedule.orderConfirm()
+// schedule.orderConfirm()
